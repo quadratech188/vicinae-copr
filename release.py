af1c481f448e75c9f0b50f0cf918a87a2b216f76
Quadratech188 <quadratech188@gmail.com>
2025-12-04 10:55:48 +0900

    feat: Add version checking

--- release.py
+++ release.py
@@ -18,6 +18,15 @@ def prepare_files(version: str):
     if prev_version_match is None:
         raise Exception('Failed to get version')
     prev_version = prev_version_match.group('version')
+    
+    prev_v = list(map(int, prev_version.split('.')))
+    v = list(map(int, version.split('.')))
+
+    if len(prev_v) != len(v):
+        raise Exception('Version formats don\'t match')
+
+    if prev_v >= v:
+        raise Exception('Version decreased')
 
     spec_text = spec_text.replace(f'Version: {prev_version}', f'Version: {version}')
     spec_text = re.sub('Release: .+\n', 'Release: 0%{?dist}\n', spec_text)
@@ -36,21 +45,24 @@ def prepare_files(version: str):
     print('Get', url)
     _ = urllib.request.urlretrieve(url, f'./v{version}.tar.gz')
 
-import sys
-import subprocess
-version = sys.argv[1]
+def update(version: str):
+    import subprocess
 
-prev_hash = subprocess.run(['git', 'rev-parse', 'HEAD'], capture_output=True, text=True) \
-        .stdout.rstrip('\n')
-print('Prev hash:', prev_hash)
+    prev_hash = subprocess.run(['git', 'rev-parse', 'HEAD'], capture_output=True, text=True) \
+            .stdout.rstrip('\n')
+    print('Prev hash:', prev_hash)
 
-try:
-    prepare_files(version)
-    _ = subprocess.run(['git', 'add', '.'])
-    _ = subprocess.run(['git', 'commit', '-m', f'chore: Bump to v{version}'])
-    _ = subprocess.run(['tito', 'tag', '--accept-auto-changelog'])
-    _ = subprocess.run(['git', 'push', '--follow-tags'])
-except Exception as e:
-    print('Error occured:', e)
-    _ = subprocess.run(['git', 'reset', '--hard', prev_hash])
-    exit(1)
+    try:
+        prepare_files(version)
+        _ = subprocess.run(['git', 'add', '.'])
+        _ = subprocess.run(['git', 'commit', '-m', f'chore: Bump to v{version}'])
+        _ = subprocess.run(['tito', 'tag', '--accept-auto-changelog'])
+        _ = subprocess.run(['git', 'push', '--follow-tags'])
+    except Exception as e:
+        _ = subprocess.run(['git', 'reset', '--hard', prev_hash])
+        raise e
+
+if __name__ == '__main__':
+    import sys
+    version = sys.argv[1]
+    update(version)
